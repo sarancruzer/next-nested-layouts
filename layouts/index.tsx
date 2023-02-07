00e51c2c4a6eac379d1e2441394b9ee24ab62296
sarancruzer <sarancruzer@gmail.com>
2023-02-07 16:53:40 +0530

    4 files modified

--- layouts/index.tsx
+++ layouts/index.tsx
@@ -1,5 +1,5 @@
 export function IndexLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="m-4 p-4 border-[16px] border-green-300">{children}</main>
+    <main className="m-4 p-4 border-[5px] border-blue-300">{children}</main>
   );
 }
